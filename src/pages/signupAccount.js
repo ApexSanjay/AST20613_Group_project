d0d128152b48f53423e1e97e1c7382ed20e7064b
Jacky Fan <mrf1013@outlook.com>
2021-03-09 21:30:38 +0800

    implement signup function

--- src/pages/signupAccount.js
+++ src/pages/signupAccount.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import Button from '@material-ui/core/Button';
@@ -9,66 +9,92 @@ import styled from 'styled-components';
 
 import MenuBar from "./components/menuBarBeforeSignin";
 
+import LoginModules from "./modules/LoginModules";
+
 function SignupAccount(props) {
+
+    var name = "", email = "", password = "";
+    const [error, setError] = useState("");
 
     const Container = styled.div`
         margin: auto;
         width: 80%;
     `;
-
-    //btn handler
+    
     const history = useHistory();
-    const btnHandler = (btnName) => {
-        switch (btnName) {
-            case "continue":
-                history.push("/signup/payment");
+
+    //onchange handler
+    const onChangeHandler = (field, value) => {
+        switch (field) {
+            case "name":
+                name = value;
+                break;
+            case "email":
+                email = value;
+                break;
+            case "password":
+                password = value;
                 break;
 
             default:
                 break;
         }
-    }
+        // console.log(name, email, password);
+    };
 
     const AccountForm = () => {
-        const Form = styled.form`
+
+        const Table = styled.table`
             margin: auto;
             padding: 5%;
             text-align: center;
             width: 40%;
         `;
 
-        const Table = styled.table`
-            width: 100%;
-        `;
-
         return (
-            <Form>
-                <Table>
-                    <tr>
-                        <td>Email</td>
-                        <td>
-                            <TextField
-                                id="outlined-basic"
-                                label="Email"
-                                variant="outlined"
-                                fullWidth
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Password</td>
-                        <td>
-                            <TextField
-                                id="outlined-basic"
-                                label="Password"
-                                variant="outlined"
-                                type="password"
-                                fullWidth
-                            />
-                        </td>
-                    </tr>
-                </Table>
-            </Form>
+            <Table>
+                <tr>
+                    <td>Name</td>
+                    <td>
+                        <TextField
+                            id="name"
+                            label="Name"
+                            variant="outlined"
+                            onChange={(e) => { onChangeHandler("name", e.target.value) }}
+                            fullWidth
+                            required
+                        />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td>
+                        <TextField
+                            id="email"
+                            label="Email"
+                            variant="outlined"
+                            type="email"
+                            onChange={(e) => { onChangeHandler("email", e.target.value) }}
+                            fullWidth
+                            required
+                        />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td>
+                        <TextField
+                            id="password"
+                            label="Password"
+                            variant="outlined"
+                            type="password"
+                            onChange={(e) => { onChangeHandler("password", e.target.value) }}
+                            fullWidth
+                            required
+                        />
+                    </td>
+                </tr>
+            </Table>
         );
     }
 
@@ -77,29 +103,46 @@ function SignupAccount(props) {
             <Button
                 variant="contained"
                 color="primary"
-                onClick={() => { btnHandler("continue") }}
+                // onClick={() => { btnHandler("continue") }}
                 fullWidth
+                type="submit"
             >
                 Next
             </Button>
         );
     }
 
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        LoginModules.register(email, password).then(() => {
+            LoginModules.updateName(name).then(() => {
+                history.push("/signup/payment");
+            });
+        }).catch((e) => {
+            setError(e.code + ": " + e.message);
+        });
+    }
+
     return (
         <Container>
             <MenuBar></MenuBar>
-            <Grid container>
-                <Grid item xs={12}>
-                    <h2>Sign Up Page - Account Create</h2>
+            <form onSubmit={onSubmitHandler}>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <h2>Sign Up Page - Account Create</h2>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <AccountForm />
+                    </Grid>
+                    <Grid item xs={11}></Grid>
+                    <Grid item xs={1}>
+                        <NextButton />
+                    </Grid>
+                    <Grid item xs={12}>
+                        {error}
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <AccountForm />
-                </Grid>
-                <Grid item xs={11}></Grid>
-                <Grid item xs={1}>
-                    <NextButton />
-                </Grid>
-            </Grid>
+            </form>
         </Container>
     );
 }
