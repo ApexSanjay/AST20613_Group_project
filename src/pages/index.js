61dbeb19c3b24103cb1806657639c90cf23f884e
Jacky Fan <mrf1013@outlook.com>
2021-03-10 23:40:44 +0800

    added upload.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,3 +12,4 @@ export { default as Home } from './home';
 export { default as Profile } from './profile';
 export { default as Play } from './play';
 export { default as Series } from './series';
+export { default as Upload } from './upload';
