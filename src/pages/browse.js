59c3055d577b6b8d7f18ac5476323614920e3c07
Jacky Fan <mrf1013@outlook.com>
2021-03-24 20:45:36 +0800

    implement display poster in browse.js

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Link,
 } from "react-router-dom";
@@ -11,10 +11,64 @@ import MenuBar from "./components/menuBar";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 import LoginModules from './modules/LoginModules';
+import MediaModule from './modules/MediaModule';
 
 export function Browse(props) {
 
     console.log(LoginModules.getUserProfile().uid);
+
+    const [ids, setIds] = useState();
+    const [movieCardInfoState, setMovieCardInfoState] = useState([]);
+    // const [refresh, setRefresh] = useState(false);
+
+
+
+    const getRandomIds = (num) => {
+        var ids = [];
+
+        for (var i = 0; i < num; i++) {
+            var id = parseInt(Math.random() * 1000);
+            id %= 100;
+            id++;
+
+            ids.push(id);
+        }
+
+        return ids;
+    }
+
+    useEffect(() => {
+        console.log("useEffect");
+        // console.log(ids);
+
+        if (!ids) {
+            var random = getRandomIds(10);
+            setIds(random);
+        } else {
+            if (movieCardInfoState.length === 0) {
+                for (var i in ids) {
+                    MediaModule.getMoviePoster(ids[i]).then((url) => {
+                        // console.log(url);
+                        for (var j in ids) {
+                            if (url.includes(ids[j] + ".jpg")) {
+                                // console.log(ids[j], url);
+                                var movieCardInfo = movieCardInfoState;
+                                movieCardInfo.push({ id: ids[j], url: url });
+                                setMovieCardInfoState([...movieCardInfo]);
+                            }
+                        }
+                    }).catch((e) => {
+                        // console.log("no url");
+                    });
+                }
+            }
+            //     console.log(movieCardInfoState);
+            //     // setRefresh(refresh);
+            // }
+        }
+
+
+    }, [ids, movieCardInfoState]);
 
     const MovieCardRow = () => {
 
@@ -54,10 +108,24 @@ export function Browse(props) {
             );
         }
 
+        const showMovieCards = () => {
+            // var ids = getRandomIds(10);
+
+            // console.log();
+            // console.log(movieCardInfoState);
+
+
+            var res = movieCardInfoState.map((info) => (
+                <MovieCard id={info.id} img={info.url} />
+            ));
+
+            return res;
+        }
+
         return (
             <div>
                 <Splide options={Options}>
-                    <MovieCard id="1" img={moviePoster} />
+                    {/* <MovieCard id="1" img={moviePoster} />
                     <MovieCard id="2" img={moviePoster} />
                     <MovieCard id="3" img={moviePoster} />
                     <MovieCard id="4" img={moviePoster} />
@@ -65,7 +133,8 @@ export function Browse(props) {
                     <MovieCard id="6" img={moviePoster} />
                     <MovieCard id="7" img={moviePoster} />
                     <MovieCard id="8" img={moviePoster} />
-                    <MovieCard id="9" img={moviePoster} />
+                    <MovieCard id="9" img={moviePoster} /> */}
+                    {showMovieCards()}
                 </Splide>
             </div>
         );
