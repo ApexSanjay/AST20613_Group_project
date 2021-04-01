840e96aae665c3cab7e40bf270ae8e5ce44c73b8
Jacky Fan <mrf1013@outlook.com>
2021-04-01 20:43:52 +0800

    implement basic layout of manage admin page

--- frontend/src/pages/manageAdmins.js
+++ frontend/src/pages/manageAdmins.js
@@ -2,18 +2,62 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import MenuBar from "./components/menuBar";
 import Container from "./components/container";
+import { useHistory } from 'react-router';
+import { makeStyles } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 export function ManageAdmins(props) {
 
+    const history = useHistory();
+
+    const useStyles = makeStyles({
+        table: {
+            minWidth: 650,
+        },
+    });
+    const classes = useStyles();
+
     return (
         <Container>
-            {/* app bar */}
             <MenuBar />
-            <Grid container spacing={0}>
-                {/* do your things here */}
-                
-                {/* (xs sum == 12) === a row */}
-                <Grid item xs={12}> 
+            <Grid container spacing={3}>
+                <Grid item xs={11}>
+                    <h2>Manage Admins</h2>
+                </Grid>
+                <Grid item xs={1}>
+                    {/* button */}
+                </Grid>
+                <Grid item xs={12}>
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Admins</TableCell>
+                                    <TableCell align="right">Actions</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+
+
+
+                                <TableRow key="row name">
+                                    <TableCell component="th" scope="row">
+                                        Row Name
+                                        </TableCell>
+                                    <TableCell align="right">Button here</TableCell>
+                                </TableRow>
+
+
+
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </Grid>
 
             </Grid>
