91603b8e89aff7ba0527075e600c6af4f0a60a7b
Jacky Fan <mrf1013@outlook.com>
2021-04-01 20:52:58 +0800

    implement get all admin function

--- frontend/src/pages/manageMovies.js
+++ frontend/src/pages/manageMovies.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import MenuBar from "./components/menuBar";
 import Container from "./components/container";
@@ -11,7 +11,16 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import AdminModules from "./modules/AdminModules";
+import BrowsingModules from './modules/BrowsingModules';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
 
 export function ManageMovies(props) {
 
@@ -24,19 +33,120 @@ export function ManageMovies(props) {
     });
     const classes = useStyles();
 
+    const [openAdminDialog, setOpenAdminDialog] = React.useState(false);  
+
+    
+
+    const UploadMovieButton = (props) => {
+
+        const Container = styled.div`
+            display: inline;
+            margin: 5px;
+        `;
+
+        const onclickHandler = () => {
+            setOpenAdminDialog(true);
+        }
+
+        return (
+            <Container>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={onclickHandler}
+                    fullWidth
+                >
+                    Upload a new movie
+                </Button>
+            </Container>
+
+        );
+    };
+
+    const AdminDialog = () => {
+
+        var email = "";
+        var role = "";
+
+        const handleClose = () => {
+            setOpenAdminDialog(false);
+        };
+
+        const onSubmitHandler = () => {
+            console.log("Clicked");
+            if (email.length !== 0 && role.length !== 0) {
+                // console.log(email, role);
+                BrowsingModules.getUser(email).then((querySnapshot) => {
+                    if(!querySnapshot.empty){
+                        var uid;
+                        querySnapshot.forEach((doc) => {
+                            uid = doc.data().userID;
+                            AdminModules.addAdmin(uid, role).then(() => {
+                                handleClose();
+                                console.log("done");
+                            });
+                        });
+                    } else {
+                        console.log("no this user");
+                    }
+
+                }).catch((e) => {
+                    console.log(e.message);
+                });
+
+            }
+        }
+
+        return (
+            <div>
+                <Dialog open={openAdminDialog} onClose={handleClose}>
+                    <DialogTitle>Add a new Admin</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            To add a new admin to this website, please input a email address here. Please be noticed that he/she must register before becoming a admin.
+                  </DialogContentText>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            type="text"
+                            label="Role"
+                            onChange={(e) => { role = e.target.value; }}
+                            fullWidth
+                        />
+                        <TextField
+                            margin="dense"
+                            type="email"
+                            label="Email Address"
+                            onChange={(e) => { email = e.target.value; }}
+                            fullWidth
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} color="primary">
+                            Cancel
+                  </Button>
+                        <Button onClick={() => { onSubmitHandler() }} color="primary">
+                            Add
+                  </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    };
+
     return (
         <Container>
             <MenuBar />
             <Grid container spacing={3}>
-                <Grid item xs={11}>
+                <Grid item xs={10}>
                     <h2>Manage Movies</h2>
                 </Grid>
-                <Grid item xs={1}>
-                    {/* button */}
+                <Grid item xs={2}>
+                    <UploadMovieButton />
                 </Grid>
                 <Grid item xs={12}>
                     <TableContainer component={Paper}>
-                        <Table className={classes.table} aria-label="simple table">
+                        <Table className={classes.table}>
                             <TableHead>
                                 <TableRow>
                                     <TableCell>Movies</TableCell>
@@ -60,7 +170,7 @@ export function ManageMovies(props) {
                         </Table>
                     </TableContainer>
                 </Grid>
-
+                <AdminDialog />
             </Grid>
         </Container>
     );
