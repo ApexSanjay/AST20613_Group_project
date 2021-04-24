48e4fb6a6ed8f46aa593c9940114713eabad910b
Jacky Fan <mrf1013@outlook.com>
2021-04-24 21:09:17 +0800

    fix cast display error

--- frontend/src/pages/series.js
+++ frontend/src/pages/series.js
@@ -289,12 +289,18 @@ function Series(props) {
 
         const displayCast = () => {
             var res = "";
-            for (var i in movies.cast) {
-                res += movies.cast[i];
-                if (i !== (movies.cast.length - 1)) {
-                    res += ", ";
+
+            if(typeof movies.cast === "object"){
+                for (var i in movies.cast) {
+                    res += movies.cast[i];
+                    if (i !== (movies.cast.length - 1)) {
+                        res += ", ";
+                    }
                 }
+            } else {
+                res = movies.cast;
             }
+
             return res;
         }
 
